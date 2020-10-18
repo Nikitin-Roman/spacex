2d516c0194f282730405f0a18a59b00607f5cb53
Roman <nro91kh@gmail.com>
2020-10-18 14:31:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './components/header/header';
+import Main from './components/main/main';
+import Features from './components/features/features';
+import Footer from './components/footer/footer';
+import Calendar from './components/calendar/calendar';
+import Details from './components/details/details';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FetchData from './service/FetchData';
+import './style.css';
+
+class App extends Component {
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: [],
+  }
+
+  componentDidMount(){
+    this.updateRocket();
+  }
+  updateRocket(){
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({rockets: data.map(item => item.name)})
+        return data
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => this.setState({rocketFeatures}));
+     
+  }
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket);
+  }
+  render(){
+    return (
+      <React.Fragment>
+        <Header rockets = {this.state.rockets} changeRocket = {this.changeRocket} />
+        <Main rocket = {this.state.rocket} />
+        <Features />
+        <Footer />
+      </React.Fragment>
+    );    
+  }
 }
 
 export default App;
